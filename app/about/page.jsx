41c85f71a0ff775046f07f2907b47d9f1b1fd555
lenny-dev <104114128+lennycba@users.noreply.github.com>
2023-09-27 00:49:12 -0300

    avance

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./about.module.css";
+import Image from "next/image";
 
 function About() {
   return (
@@ -13,17 +14,22 @@ function About() {
           <h1>I'm Mario Lujan</h1>
           <h3>Backend Oriented Full Stack web Developer</h3>
           <p>
-            Mi amor por la tecnología me llevó a dar un giro luego
-            de graduarme como Arquitecto<br /> y afrontar un nuevo reto como <b>Full
-            Stack Web Developer.</b> <br /> 
-            Tengo fascinación por
-            la creación de sistemas eficientes, escalables y seguros<br /> que
-            respalden la funcionalidad esencial de las aplicaciones. ¡Estoy
-            ansioso por explorar<br /> nuevas oportunidades y contribuir al éxito de
-            un equipo dedicado!<br /> Si crees que podría ser una adición valiosa a tu equipo, ¡no dudes
-            en contactarte conmigo!
+            Mi amor por la tecnología me llevó a dar un giro luego de graduarme
+            como Arquitecto
+            <br /> y afrontar un nuevo reto como{" "}
+            <b>Full Stack Web Developer.</b> <br />
+            Tengo fascinación por la creación de sistemas eficientes, escalables
+            y seguros
+            <br /> que respalden la funcionalidad esencial de las aplicaciones.
+            <br />¡Estoy ansioso por explorar nuevas oportunidades y contribuir al éxito de un equipo
+            dedicado!
+            <br /> <b>Si crees que podría ser una adición valiosa a tu equipo, ¡no
+            dudes en contactarte conmigo!</b>
           </p>
         </div>
+          <div className={styles.img}>
+            <Image src="/me.jpg" width={500} height={500} alt="Me" />
+          </div>
       </div>
     </div>
   );
